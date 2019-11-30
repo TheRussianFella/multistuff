4a15940516ff77115e6491c39e4742d1aea41bc2
TheRussianFella <maximmanainen@gmail.com>
2019-11-30 13:06:03 +0300

    microshell: Tests added

--- microshell/src/shell.cpp
+++ microshell/src/shell.cpp
@@ -2,16 +2,35 @@
 
 #include "microsh.h"
 
-int main() {
+int main(int argc, char** argv) {
 
   Microsh micro;
   std::string line;
   int code = 0;
 
-  while (!code) {
+  if (argc == 1){
+    while (!code) {
+      line = micro.get_line();
+      code = micro.run(line);
+    }
+  } else if (std::string(argv[1]) == "test") {
 
-    line = micro.get_line();
-    code = micro.run(line);
+    std::vector<std::string> tests({ std::string("ls -l"),
+    std::string("ls -l *"),
+    std::string("ls -l /*"),
+    std::string("ls -l /*/*"),
+    std::string("ls -l /*/*.h"),
+    std::string("ls -l /usr/include/*.h"),
+    std::string("cat < Makefile | cat | cat | cat | cat"),
+    std::string("cat < Makefile | cat | cat | cat | cat | cat > b"),
+    std::string("cat < a < b"),
+    std::string("cat < Makefile | cat > b"),
+    std::string("set MAX=idon'tlikeyou"),
+    std::string("echo $MAX")
+  });
+
+    for (auto t: tests)
+      micro.run(t);
 
   }
 
