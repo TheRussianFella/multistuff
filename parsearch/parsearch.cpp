8e8be88c8815ecb7952a76d7fab094c71fe8c986
TheRussianFella <maximmanainen@gmail.com>
2019-12-07 14:42:28 +0300

    parsearch: multiple threads

--- parsearch/parsearch.cpp
+++ parsearch/parsearch.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <thread>
-#include <mutex>
 #include <string>
-#include <queue>
-#include <condition_variable>
 #include <future>
 #include <vector>
 #include <chrono>
 #include <assert.h>
+#include <queue>
+#include <string>
+#include <mutex>
+#include <chrono>
+#include <condition_variable>
+
 
 class ParallelQueue {
 
@@ -17,41 +20,47 @@ private:
   std::mutex queue_mutex;
   std::condition_variable not_empty;
   size_t size;
-
-  int MAX_WAIT_TIME = 500;
+  int MAX_WAIT_TIME;
 
 public:
 
-  int push(const std::string& value) {
+  ParallelQueue( int MAX_WAIT_TIME = 500 ) : MAX_WAIT_TIME(MAX_WAIT_TIME), size(0) {};
 
-    std::lock_guard<std::mutex> lck( queue_mutex );
-    queue.push( std::move(value) );
-    size += 1;
-    not_empty.notify_one();
-
-    return 0;
-
-  }
-
-  std::string pop() {
-
-    // If queue is empty we just wait for someone to add items into it.
-    std::unique_lock<std::mutex> lck( queue_mutex );
-    not_empty.wait_for(lck, std::chrono::milliseconds(MAX_WAIT_TIME), [this]()->bool{ return size; });
-
-    // We've reached time out and should quit
-    if(!size) {
-      throw std::runtime_error("timeout");
-    }
-
-    std::string result(queue.front());
-    queue.pop();
-    size -= 1;
-    return result;
-
-  }
+  int push(const std::string& value);
+  std::string pop();
 
 };
+
+
+int ParallelQueue::push(const std::string& value) {
+
+  std::lock_guard<std::mutex> lck( queue_mutex );
+  queue.push( std::move(value) );
+  size += 1;
+  not_empty.notify_one();
+
+  return 0;
+}
+
+std::string ParallelQueue::pop() {
+
+  // If queue is empty we just wait for someone to add items into it.
+  std::unique_lock<std::mutex> lck( queue_mutex );
+  not_empty.wait_for(lck, std::chrono::milliseconds(MAX_WAIT_TIME), [this]()->bool{ return size; });
+
+  // We've reached time out and should quit
+  if(!size) {
+    throw std::runtime_error("timeout");
+  }
+
+  std::string result(queue.front());
+  queue.pop();
+  size -= 1;
+  return result;
+
+}
+
+
 
 class FileMatcher {
 
@@ -60,16 +69,17 @@ public:
   void operator()(ParallelQueue& shared_queue, std::string name, std::shared_future<void> death_signal) {
 
       while ( death_signal.wait_for(std::chrono::milliseconds(1)) == std::future_status::timeout ){
-
         try {
+
           std::string file_path(shared_queue.pop());
           std::cout << name << " got " << file_path << "\n";
+
         } catch (const std::runtime_error& e){
-          std::cout << e.what() << "\n";
+
           if (e.what() != std::string("timeout"))
             throw e;
-        }
 
+        }
       }
 
       std::cout << "Thread terminated...\n";
@@ -84,8 +94,9 @@ public:
   void operator()(ParallelQueue& shared_queue) {
 
     for (int i = 0; i < 4; ++i) {
+      std::cout << "Making string...\n";
       shared_queue.push( std::string("my string ") + std::to_string(i) );
-      std::this_thread::sleep_for(std::chrono::seconds(1));
+      std::this_thread::sleep_for(std::chrono::milliseconds(300));
     }
   }
 
@@ -93,7 +104,8 @@ public:
 
 int main() {
 
-  ParallelQueue q;
+  int MAX_WAIT_TIME = 1000;
+  ParallelQueue q(MAX_WAIT_TIME);
   FileMatcher f;
 
   std::promise<void> death_signal;
@@ -103,7 +115,7 @@ int main() {
 
   std::vector <std::thread> matchers;
   for (int i = 0; i < 4; ++i) {
-    std::thread thr(f, std::ref(q), std::string("Thread ")+std::to_string(i), std::move(death_future));
+    std::thread thr(f, std::ref(q), std::string("Thread ")+std::to_string(i), death_future);
     matchers.push_back(std::move(thr));
   }
 
@@ -114,7 +126,6 @@ int main() {
 
   std::cout << "Terminating!\n";
   death_signal.set_value();
-  std::cout << "Value setted.../\n";
 
   for( size_t i = 0; i < matchers.size(); ++i)
     matchers[i].join();
